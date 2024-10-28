bd283e265d6c9a92414c6c585295bcd44bf81b5f
prestondntran <prestondnt@gmail.com>
2024-10-28 13:30:33 -0700

    testing branch: should be wrong

--- Lab 4/src/App.test.tsx
+++ Lab 4/src/App.test.tsx
@@ -18,7 +18,7 @@ test('create expense', () => {
   const newExpenseName = screen.getByText("Gambling");
   const newExpenseCost = screen.getByText("$562");
 
-  expect(newExpenseName).toBeInTheDocument();
+  expect(newExpenseName).not.toBeInTheDocument();
   expect(newExpenseCost).toBeInTheDocument();
 
   const remaining = screen.getByText("Remaining: $438");
